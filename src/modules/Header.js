895674f9b0fd007a09b3f6fa591cf947ea6c7d1d
K0rut0 <maleonkurt1@gmail.com>
2022-10-02 19:55:46 +0800

    test changes

---
+++ src/modules/Header.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import "./Header.css";
+
+function Header(){
+    return(
+        <div className='header-main'>
+            <h1 className='head-Text'>test</h1>
+            <ul className='ul-main'>
+                <li>test</li>
+                <li>test</li>
+                <li>test</li>
+                <li>test</li>
+            </ul>
+        </div>
+    )
+}
+
+export default Header;
