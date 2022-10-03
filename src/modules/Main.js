74ffc033a589e1e1a21bca9bc0ce1ad2b1bf788f
K0rut0 <maleonkurt1@gmail.com>
2022-10-03 13:04:57 +0800

    text align changes

--- src/modules/Main.js
+++ src/modules/Main.js
@@ -9,11 +9,13 @@ function Main(){
                 <img alt={"kannasip"} className="imgTest" src={Kannasip}/>
             </div>
             <div className='main-desc'>
+                <h1>Hello!</h1>
                 <p>
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus libero tortor, varius at purus vitae, ullamcorper lacinia ante. 
-                    Fusce velit lacus, rutrum sed justo non, tempor ultricies ex. Phasellus at justo leo. Nullam sit amet neque lectus. Sed ullamcorper non magna ut semper. 
-                    Nunc eget lectus convallis, rhoncus est vitae, feugiat tellus. Proin vel dui massa. Nullam at congue sem, non laoreet est. Phasellus dignissim in purus 
-                    blandit pulvinar. In eget scelerisque nunc. Duis non odio sapien. Nulla eros lacus, consectetur ut pulvinar id, placerat rhoncus turpis. Proin a 
+                    Fusce velit lacus, rutrum sed justo non, tempor ultricies ex. Phasellus at justo leo. Nullam sit amet neque lectus. Sed ullamcorper 
+                    non magna ut semper. Nunc eget lectus convallis, rhoncus est vitae, feugiat tellus. Proin vel dui massa. Nullam at congue sem, 
+                    non laoreet est. Phasellus dignissim in purus blandit pulvinar. In eget scelerisque nunc. Duis non odio sapien. 
+                    Nulla eros lacus, consectetur ut pulvinar id, placerat rhoncus turpis. Proin a 
                     vestibulum lacus. Aenean lorem eros, auctor nec ante non, vestibulum malesuada felis.
                 </p>
             </div>
