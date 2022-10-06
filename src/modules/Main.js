3e9dfc688e4d4ff6370387b87e399228758f71df
K0rut0 <maleonkurt1@gmail.com>
2022-10-06 14:51:18 +0800

    bg test

--- src/modules/Main.js
+++ src/modules/Main.js
@@ -13,14 +13,11 @@ function Main(){
                 <p>
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus libero tortor, varius at purus vitae, ullamcorper lacinia ante. 
                     Fusce velit lacus, rutrum sed justo non, tempor ultricies ex. Phasellus at justo leo. Nullam sit amet neque lectus. Sed ullamcorper 
-                    non magna ut semper. Nunc eget lectus convallis, rhoncus est vitae, feugiat tellus. Proin vel dui massa. Nullam at congue sem, 
-                    non laoreet est. Phasellus dignissim in purus blandit pulvinar. In eget scelerisque nunc. Duis non odio sapien. 
-                    Nulla eros lacus, consectetur ut pulvinar id, placerat rhoncus turpis. Proin a 
-                    vestibulum lacus. Aenean lorem eros, auctor nec ante non, vestibulum malesuada felis.
+                    non magna ut semper. 
                 </p>
             </div>
         </div>
-    )
+    )   
 }
 
 export default Main;
